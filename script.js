2c199cb7260863b9f62f3272a6c8b90cf5f5fb04
guilhermegattimarinho <ggattimarinho@gmail.com>
2023-03-25 00:31:28 -0300

    refatorando algumas funções

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 // captura de elementos
 const divColorPalette = document.querySelectorAll(".color");
+const colorSelect = document.querySelector(".selected");
 const buttonRandomColor = document.querySelector("#button-random-color");
 const buttonClearBoard = document.querySelector("#clear-board");
 const inputVqv = document.querySelector("#board-size");
@@ -18,37 +19,28 @@ const generateRandomColor = () => {
 
 // aloca cores geradas na paleta de cores
 const setAndApplyRandomColor = () => {
-  const arrayColors = ["#000000"];
+  const arrayColorsPalette = ["#000000"];
   for (let index = 2; index <= 4; index += 1) {
     const color = generateRandomColor();
     const divColor = document.getElementById(`color-${index}`);
     divColor.style.backgroundColor = color;
-    arrayColors.push(color);
+    arrayColorsPalette.push(color);
   }
-  localStorage.setItem("colorPalette", JSON.stringify(arrayColors));
+  localStorage.setItem("colorPalette", JSON.stringify(arrayColorsPalette));
 };
 
 // evento que gera as cores aleatorias
 buttonRandomColor.addEventListener("click", setAndApplyRandomColor);
-
-// salva o local storage da paleta de cores aleatorias
-const localStoragePalette = () => {
-  const colorInLocalStorage = localStorage.getItem("colorPalette");
-  const colors = colorInLocalStorage
-    ? JSON.parse(colorInLocalStorage)
-    : ["black", "red", "blue", "green"];
-  console.log(localStorage.colorPalette);
-  for (let index = 0; index < divColorPalette.length; index += 1) {
-    divColorPalette[index].style.backgroundColor = colors[index];
-  }
-};
-localStoragePalette();
+// ativa botão vqv
+buttonVqv.addEventListener("click", () => {
+  const inputVqv = document.getElementById("board-size");
+  createCellsInput(inputVqv.value);
+});
 
 // seleciona cor na paleta de cores
 const selectColor = () => {
   for (let index = 0; index < divColorPalette.length; index += 1) {
     divColorPalette[index].addEventListener("click", (event) => {
-      const colorSelect = document.querySelector(".selected");
       if (colorSelect) {
         colorSelect.classList.remove("selected");
       }
@@ -65,6 +57,57 @@ const createCells = (size) => {
     divPixelBoard.className = "pixel";
     divPixelBoard.id = index;
     grid.appendChild(divPixelBoard);
+    divPixelBoard.style.backgroundColor = "white";
   }
 };
 createCells(5);
+
+const createCellsInput = (size) => {
+  grid.innerHTML = "white";
+  if (size === "") {
+    window.alert("Board inválido!");
+    return;
+  }
+  if (size < 5) {
+    return createCells(5);
+  }
+  if (size > 50) {
+    return createCells(50);
+  }
+  createCells(size);
+};
+
+// colore os pixel de pixel board
+const paintPixel = () => {
+  grid.addEventListener("click", (event) => {
+    const colorSelected = document.querySelector(".selected");
+    // const index = event.target;
+    const paint = colorSelected.style.backgroundColor;
+    event.target.style.backgroundColor = paint;
+  });
+};
+paintPixel();
+
+// limpa o grid
+const clearGrid = () => {
+  const pixels = document.querySelectorAll(".pixel");
+  buttonClearBoard.addEventListener("click", () => {
+    for (let index = 0; index < pixels.length; index += 1) {
+      pixels[index].style.backgroundColor = "white";
+    }
+  });
+};
+clearGrid();
+
+// salva o local storage da paleta de cores aleatorias
+const localStoragePalette = () => {
+  const colorInLocalStorage = localStorage.getItem("colorPalette");
+  const colors = colorInLocalStorage
+    ? JSON.parse(colorInLocalStorage)
+    : ["black", "red", "blue", "green"];
+  console.log(localStorage.colorPalette);
+  for (let index = 0; index < divColorPalette.length; index += 1) {
+    divColorPalette[index].style.backgroundColor = colors[index];
+  }
+};
+localStoragePalette();
